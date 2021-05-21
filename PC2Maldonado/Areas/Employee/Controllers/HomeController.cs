fd3d141d61d041c7c1d5cf254b8f16452e783fc0
Paolo Maldonado <jorpal_10_20@outlook.com>
2021-05-20 20:42:28 -0500

    Add project files.

---
+++ PC2Maldonado/Areas/Employee/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PC2Maldonado.Areas.Employee.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC2Maldonado.Areas.Employee.Controllers
+{
+    [Area("Employee")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var employees = new List<employee>();
+
+            employee e1 = new employee();
+            e1.FullName = "Paolo Maldonado";
+            e1.Email = "123@hotmail.com";
+            e1.Country = "Peru";
+            e1.Age = 21;
+            e1.Id = 1;
+
+            employees.Add(e1);
+
+            return View(employees);
+        }
+    }
+}
---
+++ PC2Maldonado/Areas/Employee/Models/employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC2Maldonado.Areas.Employee.Models
+{
+    public class employee
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Country { get; set; }
+    }
+}
